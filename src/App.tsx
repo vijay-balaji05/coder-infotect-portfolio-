1686bd71d77ac34f7c8690cf7ce291b8ce26c0b7
Vijay balaji <balajibaskaran456@gmail.com>
2026-09-02 23:49:20 +0530

    chore: Init project and push to server

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { Box, Typography } from '@mui/material'
+import React from 'react'
+
+function App() {
+  return (
+    <Box>
+      <Typography sx={{ fontSize: '2rem', fontWeight: 'bold', textAlign: 'center' }}>
+        Hello, World!
+      </Typography>
+    </Box>
+  )
+}
+
+export default App
